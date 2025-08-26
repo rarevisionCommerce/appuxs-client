db11939729273ba9a64f0ec200a5ff25eb96c2ca
Dev Macharia <devmacharia7@gmail.com>
2025-08-26 22:15:51 +0300

    changed base url

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const BASE_URL = "https://ibmstest-api.febwin.co.ke";
- const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://ibmstest-api.febwin.co.ke";
+// const BASE_URL = "http://localhost:3500";
 
 export const axiosPublic = axios.create({
   baseURL: BASE_URL,
